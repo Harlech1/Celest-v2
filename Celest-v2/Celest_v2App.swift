f73803c7367e7409d1249676c4ce34b61cf7a6c4
Harlech1 <turkerkizilcik@gmail.com>
2025-06-12 21:49:59 +0300

    Celest nata est.

---
+++ Celest-v2/Celest_v2App.swift
@@ -0,0 +1,17 @@
+//
+//  Celest_v2App.swift
+//  Celest-v2
+//
+//  Created by Türker Kızılcık on 12.06.2025.
+//
+
+import SwiftUI
+
+@main
+struct Celest_v2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
